bfd085c16e2ddc1dcd2bd4c4c2c2897ee2804eb9
Auste <104443584+projectauste@users.noreply.github.com>
2024-12-01 17:57:46 +0100

    Edit to README

--- 2024/01/aoc-2024-01.py
+++ 2024/01/aoc-2024-01.py
@@ -6,13 +6,22 @@ with open('input.txt') as f:
     id_list_1 = sorted(id_list_1)
     id_list_2 = sorted(id_list_2)
 
-def add_id(id_list_1, id_list_2) -> int:
+def distance(id_list_1, id_list_2) -> int:
     if not id_list_1 or not id_list_2:
         return 0
 
-    return abs(id_list_1[0] - id_list_2[0]) + add_id(id_list_1[1:], id_list_2[1:])
+    return abs(id_list_1[0] - id_list_2[0]) + distance(id_list_1[1:], id_list_2[1:])
 
-sum = add_id(id_list_1, id_list_2)
+def similarity(id_list_1, id_list_2) -> int:
+    if not id_list_1 or not id_list_2:
+        return 0
+
+    count = id_list_2.count(id_list_1[0])
+    return count * id_list_1[0] + similarity(id_list_1[1:], id_list_2)
+
+distance_sum = distance(id_list_1, id_list_2)
+similarity_sum = similarity(id_list_1, id_list_2)
 
 with open('output.txt', 'w') as f:
-    f.write(str(sum))
+    f.write(f'P1: {distance_sum}\nP2: {similarity_sum}')
+
