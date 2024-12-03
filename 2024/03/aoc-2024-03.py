2c1a5ff395cf8557cc4e363ddacd90b9df7ec210
Austeja <a.auste@outlook.com>
2024-12-03 11:03:44 +0100

    Commit

--- 2024/03/aoc-2024-03.py
+++ 2024/03/aoc-2024-03.py
@@ -2,13 +2,28 @@ import re
 
 def main():
     with open("input.txt") as f:
-        matches = re.findall(r"mul\(\d+,\d+\)", f.read())
+        matches = re.findall(r"mul\(\d+,\d+\)|do\(\)|don't\(\)", f.read())
 
-    result = sum(eval(match) for match in matches)
+    result = add(matches)
 
     with open("output.txt", "w") as f:
         f.write(str(result))
 
+def add(matches, do=True):
+    if not matches:
+        return 0
+
+    if do == False:
+        if matches[0] == "do()":
+            do = True
+        return add(matches[1:], do)
+
+    result = eval(matches[0]) if matches[0].startswith("mul") else 0
+
+    if matches[0] == "don't()":
+        do = False
+
+    return result + add(matches[1:], do)
 
 def mul(n1, n2):
     return n1 * n2
